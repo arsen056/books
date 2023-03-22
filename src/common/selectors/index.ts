cf1127174717015036fe67404ed740ee94f8b202
arsen056 <vurdihanov@gmail.com>
2023-03-22 19:09:44 +0300

    feat: show total items

--- src/common/selectors/index.ts
+++ src/common/selectors/index.ts
@@ -3,5 +3,6 @@ import {Items} from "common/types";
 
 export const selectSearchValue = (state: RootStateType): string => state.books.searchValue
 export const selectBooks = (state: RootStateType): Items[] => state.books.items
+export const selectTotalItems = (state: RootStateType): number => state.books.totalItems
 export const selectIsLoading = (state: RootStateType): boolean => state.app.isLoading
 export const selectError = (state: RootStateType): string => state.app.error
--- src/features/books/Books.tsx
+++ src/features/books/Books.tsx
@@ -1,6 +1,6 @@
 import React, {memo} from 'react';
 import {useSelector} from "react-redux";
-import {selectBooks} from "common/selectors";
+import {selectBooks, selectTotalItems} from "common/selectors";
 import {Container} from "common/components/container";
 import {Book} from "common/components/book";
 import {SimpleGrid} from "@chakra-ui/react";
@@ -8,6 +8,7 @@ import s from './Books.module.scss'
 
 export const Books = memo(() => {
   const books = useSelector(selectBooks);
+  const totalItems = useSelector(selectTotalItems)
 
   const booksMap = books.map(book => <Book
     key={book.id}
@@ -19,6 +20,7 @@ export const Books = memo(() => {
   return (
     <main className={s.books}>
       <Container>
+        <div className={s.results}>results: {totalItems}</div>
         <SimpleGrid minChildWidth='250px' spacing='10'>
           {booksMap}
         </SimpleGrid>
