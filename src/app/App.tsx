d878c95e3771e8182997d9e70fd155fa46188057
arsen056 <vurdihanov@gmail.com>
2023-03-23 23:56:58 +0300

    refactor: use hashRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import {ProgressBar} from 'common/components/progressBar';
 import {Snackbar} from 'common/components/snackbar';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import {Routs} from 'common/routes';
 import {Header} from 'common/components/header';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app">
         <ProgressBar/>
         <Header/>
         <Routs/>
         <Snackbar/>
-
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
